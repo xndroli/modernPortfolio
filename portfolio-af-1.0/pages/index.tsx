1c703bd398ad9b4990cfa2778cb0b1b64e84f4e4
xndroli <iamsimplyclean@knights.ucf.edu>
2023-01-12 12:12:12 -0500

    verified successful install and setup of tailwind

--- portfolio-af-1.0/pages/index.tsx
+++ portfolio-af-1.0/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +10,7 @@ export default function Home() {
 				<title>Alex's Portfolio</title>
 			</Head>
 
-			<h1>Let's build an AWESOME portfolio</h1>
+			<h1 className="text-red-500">Let's build an AWESOME portfolio</h1>
 		</div>
 	);
 }
