e281148ed70bd2ed63b91acadd3eabf28a10ea78
xndroli <iamsimplyclean@knights.ucf.edu>
2023-01-13 23:56:27 -0500

    built header section

--- portfolio-af-1.0/components/Header.tsx
+++ portfolio-af-1.0/components/Header.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {};
 
 function Header({}: Props) {
 	return (
-		<header>
-			<div>{/* Social Icons */}</div>
+		<header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center padding">
+			<div className="flex flex-row items-center">
+				{/* Social Icons */}
+				<SocialIcon
+					url="https://www.youtube.com/"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://www.youtube.com/"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://www.youtube.com/"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+			</div>
+
+			<div className="flex flex-row items-center text-gray-300 cursor-pointer">
+				<SocialIcon
+					className="cursor-pointer"
+					network="email"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+
+				<p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+					Get In Touch
+				</p>
+			</div>
 		</header>
 	);
 }
